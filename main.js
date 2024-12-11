0c92315c7789fd025b74384ecf7c8d65e2f1ca1a
hpandya837 <callnirav1234@gmail.com>
2024-12-11 09:59:17 -0500

    Event Handeling

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            url: 'test.com',
-            inventory: 100,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green'},
-                { id: 2235, color: 'blue' },
+                {
+                    id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50
+                },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
-
         }
-    }
-})
+    },
+        methods: {
+            addToCart() {
+                this.cart += 1
+            },
+            updateVariant(index) {
+                this.selectedVariant = index
+            }
+        }
+    })
